f014954598db7f51dbd10d2acc3fa0e92e93a7a9
Ryan Love <mmccoopp99@googlemail.com>
2020-09-03 18:22:51 +0100

    Altered index.js to include a test document script to ensure docx.js is still working added a function for handling image to base64 instead of direct docx.js usage Added the start of an if statement in the row creation for the images to generate these fields

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs")
+var Document = require("docx").Document
+var Packer = require("docx").Packer
+var Paragraph = require("docx").Paragraph
+var TextRun  = require("docx").TextRun
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+
+
+// Create document
+  const doc = new Document();
+
+// Documents contain sections, you can have multiple sections per document, go here to learn more about sections
+// This simple example will only contain one section
+  doc.addSection({
+    properties: {},
+    children: [
+      new Paragraph({
+        children: [
+          new TextRun("Hello World"),
+          new TextRun({
+            text: "Foo Bar",
+            bold: true,
+          }),
+          new TextRun({
+            text: "\tGithub is the best",
+            bold: true,
+          }),
+        ],
+      }),
+    ],
+  });
+
+// Used to export the file into a .docx file
+  Packer.toBuffer(doc).then((buffer) => {
+    fs.writeFileSync("My Document.docx", buffer);
+  });
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -28,22 +28,24 @@ router.post("/", (req,res)=>{
        line.join().split(/End of Day [0-9]/g).pop()
        var act = line.join().split(/End of Day [0-9]/g).shift().split(/[0-9]{1,2}\.[0-9]{2}am|[0-9]{1,2}\.[0-9]{2}pm/g).splice(1,4)
 
-       /*for (let i = 0; i < act.length ; i++) {
-         for (let j = 0; j < act[i].split(",").length ; j++) {
-           console.log(clock[i] + act[i].split(",")[j])
-         }
 
-       }*/
+       function base64_encode(file) {
+         // read binary data
+         var bitmap = fs.readFileSync(file);
+         // convert binary data to base64 encoded string
+         return new Buffer(bitmap).toString('base64');
+       }
 
+       var base64str = base64_encode('photo.jpg');
+       var base64 = base64_encode('ocr.png');
 
-
-
+       const image = docx.Media.addImage(doc,base64str, 100, 100)
+       const image2 = docx.Media.addImage(doc,base64, 100, 100)
        var times = text.match(/[0-9]{1,2}\.[0-9]{2}am|[0-9]{1,2}\.[0-9]{2}pm/g).map(value => {
          return value
        })
 
-       const image = [docx.Media.addImage(doc, fs.readFileSync("C:\\Users\\Ryan Love\\WebstormProjects\\AutismCIC\\photo.jpg"), 100, 100),
-      docx.Media.addImage(doc, fs.readFileSync("C:\\Users\\Ryan Love\\WebstormProjects\\AutismCIC\\ocr.png"), 100, 100)]
+
 
       var row = []
        for (let i = 0; i < act.length ; i++) {
@@ -54,7 +56,7 @@ router.post("/", (req,res)=>{
              children: [
                new docx.TableCell({
                  children: [new docx.Paragraph({
-                   children: [image, new docx.TextRun({
+                   children: [times[i].match("11.00am") ? image2 : times[i].match("12.00pm") ? image : false , new docx.TextRun({
                      text: times[i],
                      bold: true,
                      font: "Tahoma",
@@ -63,7 +65,7 @@ router.post("/", (req,res)=>{
                }),
                new docx.TableCell({
                  children: [new docx.Paragraph({
-                   children: [image[i], new docx.TextRun({
+                   children: [act[i].split(",")[j].match("Andrew arrives") ? image : act[i].split(",")[j].match("Handover of document and products") ?  image2 : false  , new docx.TextRun({
                      text: act[i].split(",")[j],
                      bold: true,
                      font: "Tahoma",
@@ -100,99 +102,98 @@ router.post("/", (req,res)=>{
          }), new docx.Paragraph({text: `${l[0].toString()}`}), table],
        });
 
-
-
-
-
        }
      }
 
 
    if (text.split("\n").includes("Day 2")) {
 
-         var l = text.match(/Day 2/g)
-         const image = docx.Media.addImage(doc, fs.readFileSync("C:\\Users\\Ryan Love\\WebstormProjects\\AutismCIC\\ocr.png"), 100, 100);
+     if(text.match(/[0-9]{1,2}\.[0-9]{2}am|[0-9]{1,2}\.[0-9]{2}pm/g)) {
+       console.log(text.match(/[0-9]{1,2}\.[0-9]{2}am|[0-9]{1,2}\.[0-9]{2}pm/g))
+
+       var l = text.match(/Day 2/g)
+       var clock = text.match(/[0-9]{1,2}\.[0-9]{2}am|[0-9]{1,2}\.[0-9]{2}pm/g)
+       var line = text.split("\n")
+       var time = []
+       time.push(text.split(`End of Day${/[0-9]/g}`))
+       time.pop()
+       line.join().split(/End of Day [0-9]/g).pop()
+       var act = line.join().split(/End of Day [0-9]/g).shift().split(/[0-9]{1,2}\.[0-9]{2}am|[0-9]{1,2}\.[0-9]{2}pm/g).splice(1,4).filter(item => {return item})
 
 
-         const table = new docx.Table({
-           rows: [
-             new docx.TableRow({
+       function base64_encode(file) {
+         // read binary data
+         var bitmap = fs.readFileSync(file);
+         // convert binary data to base64 encoded string
+         return new Buffer(bitmap).toString('base64');
+       }
 
-               children: [
-                 new docx.TableCell({
-                   width: {size: 100 / 3, type: docx.WidthType.PERCENTAGE},
-                   children: [
-                     new docx.Paragraph({
-                       children: [
-                         new docx.TextRun({
-                           text: "Time",
-                           bold: true,
-                           font: "Tahoma",
-                         })
-                       ],
-                       alignment: docx.AlignmentType.CENTER,
-                     })
-                   ],
+       var base64str = base64_encode('photo.jpg');
+       var base64 = base64_encode('ocr.png');
 
-                 }),
-                 new docx.TableCell({
-                   width: {size: 100 / 3, type: docx.WidthType.PERCENTAGE},
-                   children: [new docx.Paragraph({
-                     children: [
-                       new docx.TextRun({
-                         text: "Activity",
-                         bold: true,
-                         font: "Tahoma",
-                       })
-                     ],
-                     alignment: docx.AlignmentType.CENTER,
-                   })],
-                 }),
-                 new docx.TableCell({
-                   width: {size: 100 / 3, type: docx.WidthType.PERCENTAGE},
-                   children: [new docx.Paragraph({
-                     children: [
-                       new docx.TextRun({
-                         text: "Place",
-                         bold: true,
-                         font: "Tahoma",
-                       })
-                     ],
-                     alignment: docx.AlignmentType.CENTER,
-                   })],
+       const image = docx.Media.addImage(doc,base64str, 100, 100)
+       const image2 = docx.Media.addImage(doc,base64, 100, 100)
+       var times = text.match(/[0-9]{1,2}\.[0-9]{2}am|[0-9]{1,2}\.[0-9]{2}pm/g).map(value => {
+         return value
+       })
 
-                 }),
-               ],
-             }),
-             new docx.TableRow({
-               children: [
-                 new docx.TableCell({
-                   children: [new docx.Paragraph({
-                     children: [image, new docx.TextRun({
-                       text: l.toString(),
-                       bold: true,
-                       font: "Tahoma",
-                     })]
-                   })],
 
-                 }),
-                 new docx.TableCell({
-                   children: [new docx.Paragraph(image)],
 
-                 }),
-                 new docx.TableCell({
-                   children: [new docx.Paragraph(image)],
+       var row = []
+       for (let i = 0; i < act.length ; i++) {
+         for (let j = 0; j < act[i].split(",").length ; j++) {
+           if (act[i].split(",")[j] === "" ) {
+             console.log(true+ act[i].split(",")[j])
+           } else {
+             console.log(false + act[i].split(",")[j])
+           }
 
-                 }),
-               ],
-             }),
-           ],
-         });
+           row.push(new docx.TableRow({
+             children: [
+               new docx.TableCell({
+                 children: [new docx.Paragraph({
+                   children: [times[i].match("11.00am") ? image2 : times[i].match("12.00pm") ? image : false , new docx.TextRun({
+                     text: times[i],
+                     bold: true,
+                     font: "Tahoma",
+                   })]
+                 })],
+               }),
+               new docx.TableCell({
+                 children: [new docx.Paragraph({
+                   children: [act[i].split(",")[j].match("Andrew arrives") ? image : act[i].split(",")[j].match("Handover of document and products") ?  image2 : false  , new docx.TextRun({
+                     text: act[i].split(",")[j] === "" ? act[i].split(",")[j].split(" "):act[i].split(",")[j] ,
+                     bold: true,
+                     font: "Tahoma",
+                   })]
+                 })],
+               }),
+               new docx.TableCell({
+                 children: [new docx.Paragraph(image)],
 
-         doc.addSection({
-           children: [ new docx.Paragraph({text: `${text[1].toString()}(${text[0].toString()})`}), table],
-         });
+               }),
+             ],
+           }))
+         }
+       }
+       console.log(row)
+
+       const table = new docx.Table({
+         rows:
+         row
+       });
+
+       doc.addSection({
+         children: [new docx.Paragraph({
+           heading: docx.HeadingLevel.HEADING_1,
+           text: "Visual Aid" // req.body.docTitle
+         }), new docx.Paragraph({text: `${l[0].toString()}`}), table],
+       });
+
      }
+
+
+ }
    if (text.split("\n").includes("Day 3")) {
 
      var l = text.match(/Day 2/g)
@@ -459,7 +460,7 @@ router.post("/", (req,res)=>{
    }
 
    docx.Packer.toBuffer(doc).then((buffer) => {
-     fs.writeFileSync(`My Document${Math.random()}.doc`, buffer);
+     fs.writeFileSync(`My Document${Math.random()}.docx`, buffer);
      res.json("DONE")
    });
 
